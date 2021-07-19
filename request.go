8c521f3f837c0f4471afcfe4d7f4b28167cc1aa4
Marcel <contact@masel.dev>
2021-07-19 23:39:37 +0000

    Implement handling request queries

--- request.go
+++ request.go
@@ -1,12 +1,13 @@
 package frankfurtergo
 
 import (
+	"fmt"
 	"net/url"
 
 	"github.com/valyala/fasthttp"
 )
 
-func (c *Client) jsonRequest(path string, queries ...[]string) (body []byte, err error) {
+func (c *Client) jsonRequest(path string, queries ...map[string]string) (body []byte, err error) {
 
 	uri := url.URL{
 		Scheme: "https",
@@ -14,6 +15,15 @@ func (c *Client) jsonRequest(path string, queries ...[]string) (body []byte, err
 		Path:   path,
 	}
 
+	if len(queries) > 0 {
+		queryValues := url.Values{}
+		for k, v := range queries[0] {
+			queryValues.Add(k, v)
+		}
+		uri.RawQuery = queryValues.Encode()
+	}
+
+	fmt.Println(uri.String())
 	status, body, err := c.httpClient.Get(nil, uri.String())
 	if err != nil {
 		return nil, err
